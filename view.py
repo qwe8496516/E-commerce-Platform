56b64ed2b92aa38aeecd08f4714cdfca4320c76d
D0957024 <jeff0984250437@gmail.com>
2022-05-25 02:50:10 +0800

    0525-2

--- view.py
+++ view.py
@@ -1,3 +1,5 @@
+from operator import ge
+from unicodedata import name
 from flask import Flask, flash
 from flask import render_template,redirect,url_for
 from flask import request
@@ -9,6 +11,16 @@ import psycopg2
 app = Flask(__name__)
 app.config['SESSION_TYPE']='filesystem'
 app.config['SECRET_KEY']=urandom(24)
+
+
+def generateId():
+    id = random.randint(111111111,999999999)
+    start = chr(random.randint(ord('A'), ord('Z')))
+    id = str(id)
+    id = start + id
+    return id
+
+
 
 @app.route('/',methods=['GET','POST'])
 def login():
@@ -60,30 +72,52 @@ def reg():
     if request.method=='GET':
         return render_template('register.html')
     elif request.method == 'POST':
-        print(request.form.get('name'))
-        print(request.form.get('phone'))
-        print(request.form.get('email'))
-        print(request.form.get('password'))
-        print(request.form.get('address'))
-        print(request.form.get('gender'))
+        name = request.form.get('name')
+        phone =request.form.get('phone')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        checkpassword = request.form.get('password_vetify')
+        address = request.form.get('address')
+        gender = request.form.get('gender')
+        if (password == checkpassword):
+            conn = psycopg2.connect(database="Database_Topic", user="postgres", password="123456789", host="127.0.0.1", port="5432")
+            id = generateId()
+            data = "select m_id from members where m_id = '{}'".format(id)
+            cur = conn.cursor()
+            cur.execute(data)
+            rows = cur.fetchall()
+            if(rows == []):
+                # 可以註冊
+                print('//////////////////////////////////////')
+                data = "Insert into members values('{}','{}','{}','{}','{}','{}','{}');".format(
+                    id,name,phone,email,gender,address,password)
+                cur.execute(data)                                                          
+            else:
+                # ID相同
+                while(True):
+                    id = generateId()
+                    data = "select m_id from members where m_id = '{}'".format(id)
+                    cur = conn.cursor()
+                    cur.execute(data)
+                    rows = cur.fetchall()
+                    if (rows == []):
+                        break
+            return redirect('/')
 
 
 
-        # conn = psycopg2.connect(database="Database_Topic", user="postgres", password="123456789", host="127.0.0.1", port="5432")
-        # x = random.randint(111111111,999999999)
-        # start = chr(random.randint(ord('A'), ord('Z')))
-        # print
-        # x = str(x)
-        # x = start + x
+        
         # data = "Insert into members values('{}','{}','{}','{}','{}','{}','{}');".format(x,request.form.get('name')   # 顯示要先改資料庫的東西
         #                                                                         ,request.form.get('my_phone'),request.form.get('email')
         #                                                                         ,request.form.get('sex'),request.form.get('address')
         #                                                                         ,request.form.get('password'))
         # cur = conn.cursor()
         # cur.execute(data)
-        return redirect('/')   
+        # return redirect('/')   
 
     
+
+
 
 
 
